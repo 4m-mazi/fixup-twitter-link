979c98756a9d5c8e149932c80d32b8f771c437db
Mogyuchi <mogyuchi@mogyuchi.jp>
2024-11-05 19:49:32 +0900

    refactor: improve implementation of commands (#765)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -7,19 +7,19 @@ import type {
 } from "@discordjs/core";
 import { deleteCommand } from "./message/delete.js";
 
-const commands = new Collection([
+const messageCommands = new Collection([
   [deleteCommand.data.name, deleteCommand],
 ]);
 
 export const commandsRegister = (({ api, applicationId }) => {
-  const body: RESTPutAPIApplicationCommandsJSONBody = commands.map(({ data }) => data);
+  const body = messageCommands.map(({ data }) => data) satisfies RESTPutAPIApplicationCommandsJSONBody;
   return api.applicationCommands.bulkOverwriteGlobalCommands(applicationId, body);
 }) satisfies (
   { api, applicationId }: { api: API; applicationId: string },
 ) => Promise<RESTPutAPIApplicationCommandsResult>;
 
 export const messageCommandsHandler = (({ interaction, api }) => {
-  return commands.get(interaction.data.name)?.handle({ interaction, api });
+  return messageCommands.get(interaction.data.name)?.handle({ interaction, api });
 }) satisfies ({ interaction, api }: {
   interaction: APIMessageApplicationCommandInteraction;
   api: API;
